fcb1d29cf3eed800dde47e672165bf254689bf77
TeeAaTeeU <teeaateeuu@gmail.com>
2014-05-08 15:25:11 +0300

    Remove not need node.js modules

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var express = require('express');
 var app     = express();
 var mongoose = require('mongoose'); // mongoose for mongodb
 var request = require('request');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
 var database = require('./config/database'); // load the database config
 
 // configuration ===========================================
@@ -12,13 +10,11 @@ mongoose.connect(database.url); // connect to mongoDB database
 
 var port = process.env.PORT || 8080; // set our port
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
-app.use(bodyParser()); // pull information from html in POST
-app.use(methodOverride()); // simulate DELETE and PUT
 
 // routes ==================================================
 require('./app/routes')(app, request); // pass our application into our routes
 
 // start app ===============================================
-app.listen(port);	
+app.listen(port);
 console.log('Magic happens (AKA static files are served) on port ' + port); // shoutout to the user
 exports = module.exports = app; // expose app
