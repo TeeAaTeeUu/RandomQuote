9c4249cdd464820346dc0fb428576afb43f7a93a
TeeAaTeeU <teeaateeuu@gmail.com>
2014-05-08 07:10:01 +0300

    Request module as dependency injection

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,10 @@
-module.exports = function(app) {
+module.exports = function(app, request) {
 
 	// server routes ===========================================================
 	// handle things like api calls
 	// authentication routes
 	app.get('/api/v1/quote/random', function(req, res) {
 
-		var request = require('request');
 		request('http://www.iheartquotes.com/api/v1/random?format=json', function (error, response, body) {
 			if (!error && response.statusCode == 200) {
     			res.send(body);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // modules =================================================
 var express = require('express');
 var app     = express();
+var request = require('request');
 
 // configuration ===========================================
 
@@ -9,7 +10,7 @@ var port = process.env.PORT || 8080; // set our port
 app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
 
 // routes ==================================================
-require('./app/routes')(app); // pass our application into our routes
+require('./app/routes')(app, request); // pass our application into our routes
 
 // start app ===============================================
 app.listen(port);	
